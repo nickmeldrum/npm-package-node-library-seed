5809342ebb21b9b7a1d8824aad92028f9761f1cd
Nick Meldrum <nick@nickmeldrum.com>
2019-03-29 09:10:06 +0000

    make template rendering recursive

--- fs.js
+++ fs.js
@@ -1,16 +1,27 @@
 const path = require('path')
+const util = require('util')
 const fsExtra = require('fs-extra')
 const mustache = require('mustache')
+
+const readdir = util.promisify(fsExtra.readdir)
 
 module.exports = config => {
   const workingDir = path.join(config.local.rootPath, config.project.repo_name)
 
   const fs = {}
 
-  const renderTemplates = async () => {
-    const file = path.join(workingDir, 'package.json')
+  const renderTemplate = async file => {
     const contents = await fsExtra.readFile(file, 'utf8')
     await fsExtra.writeFile(file, mustache.render(contents, config), 'utf8')
+  }
+
+  const renderTemplates = async (dir = workingDir) => {
+    const isDirectory = source => fsExtra.lstatSync(path.join(dir, source)).isDirectory()
+    const dirContents = await readdir(dir)
+    const dirs = dirContents.filter(isDirectory)
+    const files = dirContents.filter(item => !isDirectory(item))
+    await Promise.all(dirs.map(subDir => renderTemplates(path.join(dir, subDir))))
+    await Promise.all(files.map(file => renderTemplate(path.join(dir, file))))
   }
 
   fs.init = async () => {
